35d9f17f0da5076f714da38733b86f0490d0cce5
Marita <maritahalt96@gmail.com>
2022-10-31 14:42:36 +0000

    f{message}

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,5 +1,10 @@
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        mars();
+    }
+
+    public static void mars(){
+        System.out.println("Hello Mars");
     }
 }
